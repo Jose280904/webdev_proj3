c9b9f7ec635dadfdcbe751e842a1b30b6bc490e7
Jose280904 <jrodriguez56@hawk.iit.edu>
2024-11-13 21:24:29 -0600

    Delete map.js

--- js/map.js
+++
@@ -1,8 +0,0 @@
-function initMap() {
-  var mapOptions = {
-    center: {lat: 41.8781, lng: -87.6298},
-    zoom: 10,
-  };
-  var map = new google.maps.Map(document.getElementById("map-container"), mapOptions);
-}
-window.onload = initMap;
